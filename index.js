2bbd572c70d9bf2bc587a7714e977446b3156df2
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-07-15 14:02:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host:'localhost',
+  database: 'Mahasiswa',   // nama database
+  password: 'Razy1234',  // pw postgresql
+  port: 5432,
+});
+
+app.use(express.json());
+
+app.get('/biodata', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM biodata');
+
+    res.status(200).json({
+      message: "Berhasil mengambil data biodata",
+      data: result.rows
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
+
+//post
+
+//put
+
+//delete
